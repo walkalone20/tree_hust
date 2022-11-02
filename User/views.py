33ed72f160f10fb753f7400282f3a0e51cec3435
Huangbaobao <huangbaba@email.com>
2022-11-02 11:26:50 +0800

    hjh

--- User/views.py
+++ User/views.py
@@ -6,23 +6,23 @@ from knox.models import AuthToken
 from django.contrib.auth import login
 from User.serializer import RegistrationSerializer
 from rest_framework import generics
+import get_object_or_404()
 
 class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegistrationSerializer
 
     def post(self, request):
-        if request.method=='POST':
-            serializer=self.get_serializer(data=request.data)
-            data={}
-            if serializer.is_valid():
-                auser=serializer.save()
-                data['response']="succesfully registered a new user, u know i'm saying??"
-                data['email']=auser.email
-                data['username']=auser.username
-                data['token']=AuthToken.objects.create(auser)[1]
-            else:
-                data=serializer.errors
-            return Response(data)
+        serializer=self.get_serializer(data=request.data)
+        data={}
+        if serializer.is_valid():
+            auser=serializer.save()
+            data['response']="succesfully registered a new user, u know i'm saying??"
+            data['email']=auser.email
+            data['username']=auser.username
+            data['token']=AuthToken.objects.create(auser)[1]
+        else:
+            data=serializer.errors
+        return Response(data)
 	
 class LoginAPI(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
@@ -33,3 +33,20 @@ class LoginAPI(KnoxLoginView):
         user = serializer.validated_data['user']
         login(request, user)
         return super(LoginAPI, self).post(request, format=None)
+
+class CollectionView(APIView):
+    bad_request_message = 'An error has occurred'
+
+    def post(self, request):
+        post = get_object_or_404(Post, slug=request.data.get('slug'))
+        if request.user not in post.favourite.all():
+            post.favourite.add(request.user)
+            return Response({'detail': 'User added to post'}, status=status.HTTP_200_OK)
+        return Response({'detail': self.bad_request_message}, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        post = get_object_or_404(Post, slug=request.data.get('slug'))
+        if request.user in post.favourite.all():
+            post.favourite.remove(request.user)
+            return Response({'detail': 'User removed from post'}, status=status.HTTP_204_NO_CONTENT)
+        return Response({'detail': self.bad_request_message}, status=status.HTTP_400_BAD_REQUEST)
