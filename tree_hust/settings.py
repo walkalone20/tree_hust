c85c5a716f872a32e5cb07ee47d4e846c45b93be
Anyu10 <anyuelin10@gmail.com>
2022-11-05 02:12:52 +0800

    sd

--- tree_hust/settings.py
+++ tree_hust/settings.py
@@ -142,8 +142,8 @@ AUTH_USER_MODEL = "User.User"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
