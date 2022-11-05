5ba7deffa9b1103d41f43a2b5b1ab24545d729f6
Huangbaobao <huangbaba@email.com>
2022-11-05 19:29:49 +0800

    feiwu

--- tree_hust/settings.py
+++ tree_hust/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'HOST': '127.0.0.1', # 数据库地址，本机 ip 地址 127.0.0.1
         'PORT': 3306, # 端口
         'USER': 'root',  # 数据库用户名
-        'PASSWORD': 'AnyuElin10', # TODO:数据库密码
+        'PASSWORD': '123456', # TODO:数据库密码
     }  
 }
 
