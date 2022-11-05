7f58fdb8fb9a208b0536b2501bbc90b6e2620d25
Anyu10 <anyuelin10@gmail.com>
2022-11-05 21:47:55 +0800

    resolve migrations problems

--- tree_hust/settings.py
+++ tree_hust/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
     'default': 
     { 
         'ENGINE': 'django.db.backends.mysql',    # 数据库引擎
-        'NAME': 'treehust', # TODO:数据库名称
+        'NAME': 'tree_hust', # TODO:数据库名称
         'HOST': '127.0.0.1', # 数据库地址，本机 ip 地址 127.0.0.1
         'PORT': 3306, # 端口
         'USER': 'root',  # 数据库用户名
