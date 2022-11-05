d1341f33c7cee6f4e20a5e9a9e62a377bfa5b43a
Anyu10 <anyuelin10@gmail.com>
2022-11-05 22:31:06 +0800

    format

--- Post/urls.py
+++ Post/urls.py
@@ -6,28 +6,31 @@ from .views import UpdatePostView, MyPostView, CommentPostView, VotePostView, De
 
 #URLConf
 urlpatterns = [
+    # * CRUD on post
     path('post/', SkimPostView.as_view(), name='skim-post'),
     path('post/my', MyPostView.as_view(), name='my-post'),
     path('post/create/', CreatePostView.as_view(), name='create-post'),
-
     path('post/<int:pk>/delete/', DeletePostView.as_view(), name='delete-post'),
     path('post/<int:pk>/', OpenPostView.as_view(), name='open-post'),
     path('post/<int:pk>/update/', UpdatePostView.as_view(), name='update-post'),
 
+    # * upvote and downvote on post
     path('post/<int:pk>/upvote/', VotePostView.as_view(), name='upvote-post'),
     path('post/<int:pk>/downvote/', VotePostView.as_view(), name='downvote-post'),
 
-
+    # * post collections
+    path('collect_post/', CollectionView.as_view()),
+    path('skim_collect_post/', CollectionListView.as_view()),
+    
+    # * post browser history
+    path('skim_browser_post/', BrowserListView.as_view()),
+    
+    # * comment implementation
     path('post/<int:pk>/comment/', CommentPostView.as_view(), name='comment-post'),
     path('post/<int:pk>/comment/<int:on>/delete', DeleteCommentView.as_view(), name='delete-comment'),
     path('post/<int:pk>/comment/<int:on>/vote', VoteCommentView.as_view(), name='vote-comment'),
 
-
-    path('collect_post/', CollectionView.as_view()),
-    path('skim_collect_post/', CollectionListView.as_view()),
-
-    path('skim_browser_post/', BrowserListView.as_view()),
-
+    # * draft implementation
     path('draft/', SkimDraftView.as_view(), name='skim-draft'),
     path('draft/<int:pk>/', OpenDraftView.as_view(), name='open-draft'),
     path('draft/create/', CreateDraftView.as_view(), name='create-draft'),
