402b466f9eea0072c572a91dda8d296a04667231
Huangbaobao <huangbaba@email.com>
2022-11-05 23:59:27 +0800

    asfaf

--- Post/admin.py
+++ Post/admin.py
@@ -7,10 +7,13 @@ admin.site.register(Comment)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display=['id','posted_by_username','post_title','likes','voted_by']
+    list_display=['id','posted_by_username','post_title','likes','upvoted_by','downvoted_by']
 
     def posted_by_username(self, request):
         return request.posted_by.username
 
-    def voted_by(self, request):
-        return "\n".join([p.username for p in request.vote.all()])
+    def upvoted_by(self, request):
+        return "\n".join([p.username for p in request.upvote.all()])
+
+    def downvoted_by(self, request):
+        return "\n".join([p.username for p in request.downvote.all()])
