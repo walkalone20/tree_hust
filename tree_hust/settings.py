896ee6be79f6c43743de899103a5d81643cd01a9
Anyu10 <anyuelin10@gmail.com>
2022-11-08 11:50:15 +0800

    rubbish venv

--- tree_hust/settings.py
+++ tree_hust/settings.py
@@ -83,25 +83,25 @@ WSGI_APPLICATION = "tree_hust.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # ^ use mysql instead of sqlite
-DATABASES = { 
-    'default': 
-    { 
-        'ENGINE': 'django.db.backends.mysql',    # 数据库引擎
-        'NAME': 'treehust', # 数据库名称
-        'HOST': '127.0.0.1', # 数据库地址，本机 ip 地址 127.0.0.1
-        'PORT': 3306, # 端口
-        'USER': 'root',  # 数据库用户名
-        'PASSWORD': '123456', # 数据库密码
-    }  
-}
+# DATABASES = { 
+#     'default': 
+#     { 
+#         'ENGINE': 'django.db.backends.mysql',    # 数据库引擎
+#         'NAME': 'treehust', # 数据库名称
+#         'HOST': '127.0.0.1', # 数据库地址，本机 ip 地址 127.0.0.1
+#         'PORT': 3306, # 端口
+#         'USER': 'root',  # 数据库用户名
+#         'PASSWORD': '123456', # 数据库密码
+#     }  
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
